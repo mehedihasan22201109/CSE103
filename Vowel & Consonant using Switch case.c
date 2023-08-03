6dbaa99bb8607e3ba44d5f8ce5e4741677a18e2e
Saswata Ghosal <140143035+SaswataGA@users.noreply.github.com>
2023-08-03 18:40:57 +0600

    Add files via upload

---
+++ Vowel & Consonant using Switch case.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main ()
+{
+    int ch;
+    printf("Enter The Word here:");
+    scanf("%d",&ch);
+    switch (ch)
+    {
+    case 'a':
+        printf("Vowel");
+        break;
+    case 'e':
+        printf("Vowel");
+        break;
+    case 'i':
+        printf("Vowel");
+        break;
+    case 'o':
+        printf("Vowel");
+        break;
+    case 'u':
+        printf("Vowel");
+        break;
+    default:
+        printf("Consonant");
+        break;
+    }
+
+}
