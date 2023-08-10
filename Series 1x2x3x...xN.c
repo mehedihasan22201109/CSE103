953b6f169197f727068b5f54408b4e33a0730a54
Saswata Ghosal <140143035+SaswataGA@users.noreply.github.com>
2023-08-10 19:56:08 +0600

    Series 1x2x3x...xN.c

--- Series 1x2x3x...xN.c
+++ Series 1x2x3x...xN.c
@@ -2,17 +2,17 @@
 int main ()
 {
 
-    int i,s,t,N;
-    s=1;
+    int i,m,t,N;
+    m=1;
     printf("Enter The Number: ");
     scanf("%d",&N);
     for(i=1; i<=N; i++)
     {
         t=i;
-        s=s*t;
+        m=m*t;
 
     }
-    printf(" SUM = %d ",s);
+    printf(" Multipication = %d ",m);
 
 }
 
