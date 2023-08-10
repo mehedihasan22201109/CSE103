3eb4788ea4085faf5e75851b82acb11293b9be55
Saswata Ghosal <140143035+SaswataGA@users.noreply.github.com>
2023-08-10 19:09:20 +0600

    Add files via upload

---
+++ Odd Numbers in CONTINUE.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+
+    int i,N;
+    printf("Enter The Number: ");
+    scanf("%d",&N);
+    for(i=1; i<=N; i++)
+    {
+        if(i%2==0)
+            continue;
+        printf(" %d ",i);
+    }
+}
+
