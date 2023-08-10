48edbd9ee8c17454db79ddefa8a4d5da3921b29e
Saswata Ghosal <140143035+SaswataGA@users.noreply.github.com>
2023-08-10 19:15:51 +0600

    Add files via upload

---
+++ Series 1+2+..+N.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+
+    int i,s,t,N;
+    s=0;
+    printf("Enter The Number: ");
+    scanf("%d",&N);
+    for(i=1; i<=N; i++)
+    {
+        t=i;
+        s=s+t;
+
+    }
+    printf(" SUM = %d ",s);
+
+}
