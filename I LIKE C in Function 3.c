eb7c54d5c83876dd809eeb5f916131101d25a688
Saswata Ghosal <140143035+SaswataGA@users.noreply.github.com>
2023-08-14 15:32:53 +0600

    Add files via upload

---
+++ I LIKE C in Function 3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+LIKE(int n)
+{
+    int i;
+    for(i=1;i<=n;i++)
+    printf("Like ");
+
+}
+main ()
+{
+    int x;
+scanf("%d",&x);
+    printf("I ");
+
+    LIKE(x);
+    printf(" C.");
+}
+
